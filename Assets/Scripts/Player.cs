e1adf7d640ad3ae6f36cb5c62c4291fc66f4b8a1
gumgl <guillaume@labran.ch>
2015-02-17 22:43:06 -0500

    Create Player class

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour
+{
+	private GameObject currGame;
+	private HashSet<Village> villages;
+	private bool isActive = false;
+	private int wins = 0;
+	private int losses = 0;
+	//TODO initialize the villages
+	void Start ()
+	{
+
+	}
+
+	void Update ()
+	{
+
+	}
+
+	public void setActive() {
+
+	}
+
+	public HashSet<Village> getVillages() {
+		return villages;
+		//TODO set as readOnly
+	}
+
+	public void takeTurn() {
+
+	}
+
+	public void addVillage(Village v) {
+		villages.Add(v);
+	}
+
+	public void removeVillage(Village v) {
+		villages.Remove(v);
+	}
+
+	public void incrementWins() {
+		wins ++;
+	}
+
+	public void incrementLosses() {
+		losses ++;
+	}
+}
+
