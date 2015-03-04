09a624b705a30baf0d9f96699c04ac891ac4ef52
Guillaume <gumgl@users.noreply.github.com>
2015-03-03 19:46:32 -0500

    Import Photon Unity Networking plugin w/o demos

---
+++ Assets/Scripts/Networking.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using ExitGames.Client.Photon;
+
+public class Networking : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		ConnectUsingSettings
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
