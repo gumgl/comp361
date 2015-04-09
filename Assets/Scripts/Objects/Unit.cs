634052702ed5c9cc0e50895a64fc09f48b4cd362
alibhojani <ali.bhojani@mail.mcgill.ca>
2015-04-09 17:23:46 -0400

    Move To Error Fixed.

--- Assets/Scripts/Objects/Unit.cs
+++ Assets/Scripts/Objects/Unit.cs
@@ -300,6 +300,7 @@ public class Unit : Photon.MonoBehaviour {
 
 	public void MoveTo(Tile target) {
 		
+		if (this.getUnitType () == UnitType.Peasant && target.getOwner () != this.getOwner () && target.getOwner () != null) { 
 			board.setErrorText ("Peasants cannot invade enemy territory");
 			board.selectedUnit = null;
 			halo.SetActive(false); 
